3a02bbc5f2f51b3b86528987ee0bbf3befedc60e
Finoana-Velia <fypatrick20@yahoo.com>
2025-01-07 17:07:05 +0300

    feat : create all necessary module for the front part make the first code for home page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './layout/main/main.component';
+import { ProductListComponent } from './feature/product/product-list/product-list.component';
 
 export const routes: Routes = [
-    { path : "", component : MainComponent},
+    { path : "", component : ProductListComponent},
 ];
---
+++ client/src/app/feature/product/product-list/product-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavigationComponent } from '../../../layout/navigation/navigation.component';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [
+    NavigationComponent
+  ],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+
+}

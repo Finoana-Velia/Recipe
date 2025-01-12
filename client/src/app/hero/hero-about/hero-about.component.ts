84f905440aa2dea18c92ba950907b8c69d7cc159
Finoana-Velia <fypatrick20@yahoo.com>
2025-01-12 20:31:40 +0300

    chore : imlements font-awesome for icons

--- client/src/app/hero/hero-about/hero-about.component.ts
+++ client/src/app/hero/hero-about/hero-about.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Product } from '../../feature/product/product-modal/product';
 import { NgForOf } from '@angular/common';
 
 @Component({
@@ -11,7 +10,7 @@ import { NgForOf } from '@angular/common';
   styleUrl: './hero-about.component.css'
 })
 export class HeroAboutComponent {
-  product : Product[] = [
+  product : any[] = [
     {
       name : "Coca Cola",
       price : 1500.00
--- client/src/app/hero/hero-popular/hero-popular.component.ts
+++ client/src/app/hero/hero-popular/hero-popular.component.ts
@@ -1,10 +1,9 @@
-import { NgForOf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero-popular',
   imports: [
-    NgForOf
+
   ],
   templateUrl: './hero-popular.component.html',
   styleUrl: './hero-popular.component.css'

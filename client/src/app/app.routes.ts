d9eb3d8f693db04fc4b0d7b40aaea3b38e838877
Finoana-Velia <fypatrick20@yahoo.com>
2025-01-15 16:04:07 +0300

    feat : create login and sign in component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './layout/main/main.component';
 import { MainHeroComponent } from './hero/main-hero/main-hero.component';
+import { AuthComponent } from './core/components/auth/auth.component';
+import { RegisterComponent } from './core/components/register/register.component';
 
 export const routes: Routes = [
     { path : "", component : MainHeroComponent},
+    { path : "auth", component : AuthComponent},
+    { path : "register", component : RegisterComponent}
 ];
--- client/src/app/layout/navigation/navigation.component.ts
+++ client/src/app/layout/navigation/navigation.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
+
+  constructor(private router : Router){}
+
+  register(){
+    this.router.navigate(['register']);
+  }
 
 }

50ee941103aa3827deaa3eab71976e53343b3f3a
Finoana-Velia <fypatrick20@yahoo.com>
2025-01-21 08:48:49 +0300

    feat : create dashboard for admin interface

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { MainComponent } from './layout/main/main.component';
 import { MainHeroComponent } from './hero/main-hero/main-hero.component';
 import { AuthComponent } from './core/components/auth/auth.component';
 import { RegisterComponent } from './core/components/register/register.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path : "", component : MainHeroComponent},
     { path : "login", component : AuthComponent},
     { path : "register", component : RegisterComponent},
-    { path : "auth", component : MainComponent}
+    { path : "auth", component : MainComponent,
+        children : [
+            { path : '', component : DashboardComponent}
+        ]
+    }
 ];
--- client/src/app/layout/main/main.component.ts
+++ client/src/app/layout/main/main.component.ts
@@ -1,12 +1,13 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   imports: [
     RouterLink,
-    NgIf
+    NgIf,
+    RouterOutlet
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'

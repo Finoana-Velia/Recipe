55a43357cbfa4246dc70f0951ccced87b2ea1bef
Finoana-Velia <fypatrick20@yahoo.com>
2025-03-13 21:13:53 +0300

    feat : transfert navigator client component to the main client and delete it

--- client/src/app/client/components/main-client/main-client.component.ts
+++ client/src/app/client/components/main-client/main-client.component.ts
@@ -1,18 +1,83 @@
 import { Component } from '@angular/core';
-import { NavigationClientComponent } from '../navigation-client/navigation-client.component';
 import { FooterComponent } from '../../../layout/footer/footer.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { RecipeService } from '../../services/recipe.service';
+import { NotificationService } from '../../../core/services/notification.service';
+import { ChefService } from '../../../admin/service/chef.service';
+import { ProductService } from '../../../admin/service/product.service';
+import { NgClass, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-main-client',
   imports: [
-    NavigationClientComponent,
+    NgClass,
+    NgForOf,
     FooterComponent,
-    RouterOutlet
+    RouterOutlet,
+    RouterLink
   ],
   templateUrl: './main-client.component.html',
   styleUrl: './main-client.component.css'
 })
 export class MainClientComponent {
+menu = false;
+  notification = false;
+  cartContent = false;
+  idUser : number = 1;
+  notifList! : any;
+  isAll : boolean = false;
+
+  constructor(
+    private recipeService : RecipeService,
+    private notificationService : NotificationService,
+    private productService : ProductService,
+    private chefService : ChefService
+  ){}
+
+  ngOnInit(): void {
+    this.notificationService.notificationForClient(0,5,1).subscribe(
+      response => this.notifList = response.content
+    );
+  }
+
+  toggleMenu() {
+    this.menu = !this.menu;
+    this.notification = false;
+  }
+
+  toggleNotification() {
+    this.notification = !this.notification;
+    this.menu = false;
+  }
+
+  imageNotified(type : string,id : number) {
+    if(type === "PRODUCT") {
+      return this.productService.getImage(id);
+    }else {
+      return this.chefService.findProfile(id);
+    }
+  }
+
+  imageClassNotified(type : string) {
+    const mainClass = 'w-full h-full object-cover object-center';
+    if(type === 'PRODUCT') {
+      return mainClass + " rounded";
+    }else {
+      return mainClass + " rounded-full";
+    }
+  }
+
+  viewAll() {
+    this.isAll = !this.isAll;
+    this.notificationService.notificationForClient(0,0,1).subscribe(
+      response => this.notifList = response.content
+    );
+  }
+
+  viewLess() {
+    this.isAll = !this.isAll
+    this.ngOnInit();
+  }
+
 
 }

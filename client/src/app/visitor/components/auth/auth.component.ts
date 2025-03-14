95807fcca53e916e679af55e7d901c3416ffcf7e
Finoana-Velia <fypatrick20@yahoo.com>
2025-03-14 11:23:18 +0300

    chore (back) ; add java mail sender dependancy feat (front) : change routerlink in the register/auth/navigation components

--- client/src/app/visitor/components/auth/auth.component.ts
+++ client/src/app/visitor/components/auth/auth.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from '../../../layout/navigation/navigation.component';
 import { FooterComponent } from '../../../layout/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   imports: [
     NavigationComponent,
-    FooterComponent
+    FooterComponent,
+    RouterLink
   ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'

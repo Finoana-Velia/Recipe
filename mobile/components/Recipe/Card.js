8eb5f8113b6b86a90010126d1c956ef4d84f025c
Finoana-Velia <fypatrick20@yahoo.com>
2025-07-14 13:56:38 +0300

    feat (mobile) : allowed to add to favorite in Card component update recipe favorite card

--- mobile/components/Recipe/Card.js
+++ mobile/components/Recipe/Card.js
@@ -5,51 +5,74 @@ import Cached from "../../helpers/Image";
 import { recipeImage } from "../../API/RecipeAPI";
 import { HeartIcon } from "react-native-heroicons/outline";
 import { useState } from "react";
+import { connect } from "react-redux";
 
-export default function Card({item, navigation}) {
-    
-    let pair = item.i%2 == 0;
-    let impair = item.i%3 == 0;
+function Card(props) {
+    console.log("");
+    console.log(props);
+    console.warn("Dispatch");
+    console.log(props.dispatch);
+    console.warn("favorites");
+    console.log(props.favorites);
+    console.warn("Item");
+    console.log(props.item);
+    console.warn("navigation");
+    console.log(props.navigation);
+    console.log("");
+    // let pair = item.i%2 == 0;
+    // let impair = item.i%3 == 0;
 
-    const recipe = item.item
+    // const recipe = item.item
 
-    const [isFavorite, setFavorite] = useState(false);
+    // const [isFavorite, setFavorite] = useState(false);
 
-    return (
-        <Animated.View
-            entering={FadeInDown.delay(item.i*100).duration(600).springify().damping(12)}
-            style={{
-                flex : 1,
-                width : '100%',
-                paddingLeft : pair ? 0 : 8,
-                paddingRight : pair ? 8 : 0,
-            }}
-                sharedTransitionStyle={recipe.name}
-        >
-            <TouchableOpacity
-                onPress={() => setFavorite(!isFavorite)}
-                style={tw`w-7 h-7 bg-white absolute top-3 right-5 z-100 rounded-full flex justify-center items-center`}
-            >
-                <HeartIcon size={20} strokeWidth={4} style={isFavorite ? tw`text-red-500` : tw`text-slate-500`}/>
-            </TouchableOpacity>
-            <TouchableOpacity
-                // onPress={() => navigation.navigate('Detail', recipe.id)}
-                onPress={() => navigation.navigate('Detail',recipe)}
-            >
+    // const toggleFavorite = () => {
+    //     const action = { type : 'TOGGLE_FAVORITE', value : recipe};
+        
+    // }
+
+    // return (
+    //     <Animated.View
+    //         entering={FadeInDown.delay(item.i*100).duration(600).springify().damping(12)}
+    //         style={{
+    //             flex : 1,
+    //             width : '100%',
+    //             paddingLeft : pair ? 0 : 8,
+    //             paddingRight : pair ? 8 : 0,
+    //         }}
+    //             sharedTransitionStyle={recipe.name}
+    //     >
+    //         <TouchableOpacity
+    //             onPress={toggleFavorite}
+    //             style={tw`w-7 h-7 bg-white absolute top-3 right-5 z-100 rounded-full flex justify-center items-center`}
+    //         >
+    //             <HeartIcon size={20} strokeWidth={4} style={isFavorite ? tw`text-red-500` : tw`text-slate-500`}/>
+    //         </TouchableOpacity>
+    //         <TouchableOpacity
+    //             // onPress={() => navigation.navigate('Detail', recipe.id)}
+    //             onPress={() => navigation.navigate('Detail',recipe)}
+    //         >
                 
-                    <Cached
-                        decorator={{
-                            width : '100%',
-                            height : impair ? 150 : 250,
-                            backgroundColor : 'silver',
-                            borderRadius : 35,
-                        }}
-                        url={{uri : recipeImage(recipe.id)}}
-                    />
-                    <Text style={tw`m-1`}>{recipe.name.length > 20 ? recipe.name.slice(0,10) + '...' : recipe.name}</Text>
-            </TouchableOpacity>
-        </Animated.View>
-    )
+    //                 <Cached
+    //                     decorator={{
+    //                         width : '100%',
+    //                         height : impair ? 150 : 250,
+    //                         backgroundColor : 'silver',
+    //                         borderRadius : 35,
+    //                     }}
+    //                     url={{uri : recipeImage(recipe.id)}}
+    //                 />
+    //                 <Text style={tw`m-1`}>{recipe.name.length > 20 ? recipe.name.slice(0,10) + '...' : recipe.name}</Text>
+    //         </TouchableOpacity>
+    //     </Animated.View>
+    // )
 };
 
 
+const mapStateToProps = (state) => {
+    return {
+        favorites : state.favorites
+    }
+}
+
+export default connect(mapStateToProps)(Card);
--- mobile/screens/Favorite.js
+++ mobile/screens/Favorite.js
@@ -4,44 +4,55 @@ import tw from 'twrnc';
 import Header from '../components/Header';
 import Cached from '../helpers/Image';
 import { recipeImage } from '../API/RecipeAPI';
-import { HeartIcon, ShoppingCartIcon } from 'react-native-heroicons/outline';
+import { EyeIcon, HeartIcon, ShoppingCartIcon } from 'react-native-heroicons/outline';
+import { useNavigation } from '@react-navigation/native';
 
 function Favorite(props) {
 
-    const showStoreContent = () => {
-        console.log("props.favorites");
-        console.log(props.favorites);
-    }
+    const navigation = useNavigation();
+
+    const displayFavorite = ({item}) => (
+        <View style={tw`flex flex-row gap-2 border-b border-slate-200 py-2`}>
+          <Cached 
+            decorator={{
+                width : 100,
+                height : 100,
+                borderRadius : 10
+            }}
+            url={{ uri : recipeImage(item.id)}}
+          />
+          <View style={tw`flex w-full gap-2`}>
+            <Text style={tw`text-lime-500 font-bold text-xl`}>{item.name}</Text>
+            <Text style={tw`text-md text-slate-500 font-bold`}>{item.category}</Text>
+            <View style={tw`flex flex-row w-full gap-5`}>
+                <TouchableOpacity>
+                    <HeartIcon size={25} strokeWidth={2} style={tw`text-red-500`}/>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('Detail',item)}
+                >
+                    <EyeIcon size={25} strokeWidth={2} style={tw`text-lime-500`}/>
+                </TouchableOpacity>
+            </View>
+          </View>
+          
+        </View>
+    );
 
     return (
-        // <View style={tw`flex justify-center items-center`}>
-        //     <Text>Favorite screen!</Text>
-        //     <TouchableOpacity
-        //         onPress={showStoreContent}
-        //         style={tw`p-2 bg-blue-500`}
-        //     >
-        //         <Text style={tw`text-white font-bold`}>Store</Text>
-        //     </TouchableOpacity>
-        // </View>
-        // <ScrollView
-        //     style={tw`mt-10 px-2`}
-        //     showsVerticalScrollIndicator={false}
-        // >
         <View style={tw`mt-10 px-2`}>
             <Header />
             <Text style={tw`text-center text-lime-500 text-2xl font-bold`}>Favorites</Text>
             {props.favorites.length !== 0 ? 
-            <FlatList
-                // style={tw`flex my-2`}
+            <FlatList 
                 data={props.favorites}
-                renderItem={(recipe) => {
-                    <Text>recipe name</Text>
-                }}
-            /> 
+                style={tw`pt-2`}
+                renderItem={displayFavorite}
+                keyExtractor={item => item.id}
+            />
             : <View style={tw`w-full h-[50%] flex justify-center items-center`}>
                     <Text>No more favorite recipes yet</Text>
                 </View>}
-        {/* </ScrollView> */}
         </View>
     )
 };
